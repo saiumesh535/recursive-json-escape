6b7406e31691833d0323abbe068b23403f71d5fb
saiumesh535 <d.sai535@gmail.com>
2021-09-10 16:19:49 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,57 @@
+const escapeCharacters = ["'"] as const;
+type EscapeCharactersType = typeof escapeCharacters[number];
+
+interface Response {
+    error: string;
+    result: object;
+}
+
+// returns true if input is of type object
+function checkIfObject(input: Object): boolean {
+    if (!input) {
+        return false;
+    }
+    return !(typeof input !== 'object' || (typeof input === 'object' && Array.isArray(input)))
+}
+
+function escapeCharacter(input: string): string {
+    let result = input;
+    for (const character of escapeCharacters) {
+        switch (character as EscapeCharactersType) {
+            case "'": {
+                result = result.replace(/'/g, "''");
+            }
+        }
+    }
+    return result;
+}
+
+function iterateObjectRecursively<T = Object>(input: T): T {
+    let emptyObject = { ...input };
+    // now iterate through object recursively
+    for (const [key, value] of Object.entries(input)) {
+        let isObject = checkIfObject(value);
+        if (!isObject) {
+            const replacedCharacter = escapeCharacter(value);
+            emptyObject = { ...emptyObject, [key]: replacedCharacter }
+        } else {
+            const newObject = iterateObjectRecursively(value);
+            emptyObject = { ...emptyObject, [key]: newObject };
+        }
+    }
+    return emptyObject;
+}
+
+export const escapeObject = (input: object): Response => {
+    if (!checkIfObject(input)) {
+        return {
+            error: 'Input should be an Object',
+            result: null,
+        }
+    }
+    const result = iterateObjectRecursively(input);
+    return {
+        error: null,
+        result,
+    };
+}
---
+++ tests/test.spec.ts
@@ -0,0 +1,57 @@
+import { escapeObject } from '../src/index';
+
+test('Input as an Array', () => {
+    const testObject = ['a']
+    const result = escapeObject(testObject);
+    expect(result.error).toEqual('Input should be an Object');
+});
+
+test('Sending empty string', () => {
+    const testObject = '';
+    const result = escapeObject(testObject as any);
+    expect(result.error).toEqual('Input should be an Object');
+});
+
+test('Sending null', () => {
+    const testObject = null;
+    const result = escapeObject(testObject as any);
+    expect(result.error).toEqual('Input should be an Object');
+});
+
+test('Sending empty object', () => {
+    const testObject = {};
+    const result = escapeObject(testObject);
+    expect(result.result).toEqual({});
+});
+
+test('Input as a variable type string', () => {
+    const testObject = 'test';
+    const result = escapeObject(testObject as any);
+    expect(result.error).toEqual('Input should be an Object');
+});
+
+test('Input as a variable type number', () => {
+    const testObject = 1;
+    const result = escapeObject(testObject as any);
+    expect(result.error).toEqual('Input should be an Object');
+});
+
+test('Check escape character for an Object', () => {
+    const testObject = { name: "saiumesh's" };
+    const expectedObject = { name: "saiumesh''s" };
+    const escaped = escapeObject(testObject);
+    expect(escaped.result).toEqual(expectedObject);
+});
+
+test('Check escape character for a nested object', () => {
+    const testObject = { name: "saiumesh's", data: {
+        name: "Rahul's",
+        place: "Hyderabad"
+    } };
+    const expectedObject = { name: "saiumesh''s", data: {
+        name: "Rahul''s",
+        place: "Hyderabad"
+    } };
+    const escaped = escapeObject(testObject);
+    expect(escaped.result).toEqual(expectedObject);
+});
